966ef0928c58e0f75f0961fb940b15f72f841428
Your Name <you@example.com>
2021-06-09 06:19:24 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# React-Ema-John-Server
---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+
+
+
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00.jumub.mongodb.net:27017,cluster0-shard-00-01.jumub.mongodb.net:27017,cluster0-shard-00-02.jumub.mongodb.net:27017/${process.env.DB_Name}?ssl=true&replicaSet=atlas-20e537-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const productsCollection = client.db("emaJohnStore").collection("products");
+  const ordersCollection = client.db("emaJohnStore").collection("orders");
+    
+  app.post('/addProduct', (req, res) =>{
+      const products = req.body;
+    //   console.log(products);
+    // productsCollection.insertMany(products)
+    productsCollection.insertOne(products)
+      .then(result => {
+            console.log(result.insertedCount)
+            res.send(result.insertedCount)
+      })
+  })
+
+  app.get('/products', (req, res) => {
+      productsCollection.find({})  //.limit(20)
+      .toArray( (err, documents) => {
+          res.send(documents)
+      })
+  })
+
+
+  app.get('/product/:key', (req, res) => {
+      productsCollection.find({key: req.params.key})  //.limit(20)
+      .toArray( (err, documents) => {
+          res.send(documents[0])
+      })
+  })
+
+  app.post('/productByKeys', (req, res) => {
+      const productKeys = req.body;
+      productsCollection.find({key: { $in: productKeys}})  //.limit(20)
+      .toArray( (err, documents) => {
+          res.send(documents)
+      })
+  })
+
+  app.post('/addOrder', (req, res) =>{
+    const order = req.body;
+  ordersCollection.insertOne(order)
+    .then(result => {
+          console.log(result.insertedCount)
+          res.send(result.insertedCount > 0)
+    })
+})
+
+});
+
+
+
+app.listen(5000)
